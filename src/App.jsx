46dca3df0b8fb3760853f1041c6834928a0b654a
Lisbauer <lisa.bauer@hotmail.com>
2024-06-19 02:46:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Hero from './components/Hero/Hero.jsx'
+import AboutMe from './components/AboutMe/AboutMe.jsx'
+import Projects from './components/Projects/Projects.jsx'
+
+function App() {
+
+
+  return (
+    <div>
+      <Hero />
+      <AboutMe />
+      <Projects />
+    </div>
+  )
+}
+
+export default App
